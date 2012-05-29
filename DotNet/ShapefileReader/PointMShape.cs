63d2d7cd82b3ad03ee73ab79fa47a64b098ad4b8
kristiancalhoun <kristian.calhoun@gmail.com>
2012-05-29 15:35:41 -0400

    Added PointMShape.cs

---
+++ DotNet/ShapefileReader/PointMShape.cs
@@ -0,0 +1,27 @@
+﻿using CesiumLanguageWriter;
+
+namespace ShapefileReader
+{
+    public class PointMShape : Shape
+    {
+        internal PointMShape(int recordNumber, Rectangular position, double measure)
+            : base(recordNumber, ShapeType.PointM)
+        {
+            _position = position;
+            _measure = measure;
+        }
+
+        public Rectangular Position
+        {
+            get { return _position; }
+        }
+
+        public double Measure
+        {
+            get { return _measure; }
+        }
+
+        private readonly Rectangular _position;
+        private readonly double _measure;
+    }
+}
