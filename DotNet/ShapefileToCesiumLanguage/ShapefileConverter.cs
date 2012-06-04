186d0e4998d1321318a2127aa065f7c3a67d91cd
kristiancalhoun <kristian.calhoun@gmail.com>
2012-06-04 13:38:14 -0400

    Created ShapefileToCesiumLanguage project.

---
+++ DotNet/ShapefileToCesiumLanguage/ShapefileConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CesiumLanguageWriter;
+using Shapefile;
+
+namespace ShapefileToCesiumLanguage
+{
+    public static class ShapefileConverter
+    {
+        public static void ShapefileToCesiumLanguage(CzmlDocument document)
+        {
+            
+        }
+    }
+}
