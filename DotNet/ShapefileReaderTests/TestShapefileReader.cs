270a8e56d37c36032135bb59f81b7b0d8eeb01a4
kristiancalhoun <kristian.calhoun@gmail.com>
2012-06-20 13:49:04 -0400

    Test ShapefileReader w/ a much smaller shapefile.

--- DotNet/ShapefileReaderTests/TestShapefileReader.cs
+++ DotNet/ShapefileReaderTests/TestShapefileReader.cs
@@ -12,7 +12,7 @@ namespace ShapefileReaderTests
     public class TestShapefileReader
     {
         ShapefileReader m_reader;
-        private string shapefileName = ".\\..\\..\\ExampleShapefiles\\PhiladelphiaBuildings200712.shp";
+        private string shapefileName = ".\\..\\..\\SampleShapefiles\\ne_110m_admin_0_countries.shp";
 
         public TestShapefileReader()
         {
@@ -20,29 +20,33 @@ namespace ShapefileReaderTests
         }
 
         [Test]
-        public void GetMetadataKeys()
+        public void TestGetMetadataFields()
         {
             Shape shape = m_reader[0];
             string[] keys = shape.GetMetadataFields();
             Array.Sort(keys);
-            string[] expected = new string[] { "area", "area_1", "date_updat", "elev", "fcode", "len", "perimeter", "source"};
+            string[] expected = new string[] { "abbrev", "adm0_a3", "adm0_dif", "admin", "featurecla", "fips_10_",
+                                               "gdp_md_est", "geou_dif", "geounit", "gu_a3", "iso_a2", "iso_a3", "iso_n3",
+                                               "labelrank", "level", "map_color", "name", "name_forma",
+                                               "name_sort", "pop_est", "postal", "scalerank", "sov_a3", "sovereignt",
+                                               "su_a3", "su_dif", "subunit", "terr_", "type"
+            };
             CollectionAssert.AreEqual(keys, expected);
         }
 
         [Test]
-        public void GetMetadataValue()
+        public void TestGetMetadataValue()
         {
             Shape shape = m_reader[0];
-            string value = shape.GetMetadataValue("date_updat");
-            Assert.That(value.Equals("2004"));
+            string value = shape.GetMetadataValue("abbrev");
+            Assert.That(value.Equals("Afg."));
 
-            shape = m_reader[390292];
-            value = shape.GetMetadataValue("source");
-            Assert.That(value.Equals("SANBORN"));
+            value = shape.GetMetadataValue("pop_est");
+            Assert.That(value.Equals("28400000.00"));
         }
 
         [Test]
-        public void GetRecordNumber()
+        public void TestGetRecordNumber()
         {
             Shape shape = m_reader[0];
             Assert.That(shape.RecordNumber == 1);
