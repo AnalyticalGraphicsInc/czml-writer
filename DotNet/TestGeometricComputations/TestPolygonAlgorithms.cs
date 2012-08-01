9a021f757b41cae14866158e20f59bf065938131
kristiancalhoun <kristian.calhoun@gmail.com>
2012-08-01 16:16:55 -0400

    Remove an old redundant test.

--- DotNet/TestGeometricComputations/TestPolygonAlgorithms.cs
+++ DotNet/TestGeometricComputations/TestPolygonAlgorithms.cs
@@ -126,31 +126,7 @@ namespace TestGeometricComputations
             Cartesian result = PolygonAlgorithms.IntersectPointWithRing(point, ring);
             Assert.That(result.Equals(expectedResult));
         }
-
-        [Test]
-        public void TestFindRightmostRing()
-        {
-            List<Cartesian> ring1 = new List<Cartesian> {
-                new Cartesian(0.0, 0.0, 0.0),
-                new Cartesian(0.0, 1.0, 0.0),
-                new Cartesian(1.0, 1.0, 0.0),
-                new Cartesian(1.0, 0.0, 0.0),
-            };
-
-            List<Cartesian> ring2 = new List<Cartesian> {
-                new Cartesian(2.0, 0.0, 0.0),
-                new Cartesian(2.0, 1.0, 0.0),
-                new Cartesian(3.0, 1.0, 0.0),
-                new Cartesian(3.0, 0.0, 0.0),
-            };
-            List<List<Cartesian>> rings = new List<List<Cartesian>>();
-            rings.Add(ring1);
-            rings.Add(ring2);
-            int expectedResult = 1;
-            int result = PolygonAlgorithms.GetRightmostRingIndex(rings);
-            Assert.That(result.Equals(expectedResult));
-        }
-
+        
         [Test]
         public void TestFindMutuallyVisibleVertex()
         {
