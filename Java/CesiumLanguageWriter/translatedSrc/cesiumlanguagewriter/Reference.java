533d3c70d5bf18b46e5bea3a7c087239de484e71
Matthew Amato <matt.amato@gmail.com>
2014-05-21 13:48:34 -0400

    Update Java

--- Java/CesiumLanguageWriter/translatedSrc/cesiumlanguagewriter/Reference.java
+++ Java/CesiumLanguageWriter/translatedSrc/cesiumlanguagewriter/Reference.java
@@ -4,10 +4,13 @@ package cesiumlanguagewriter;
 import agi.foundation.compatibility.*;
 import agi.foundation.compatibility.IEquatable;
 import agi.foundation.compatibility.ImmutableValueType;
+import agi.foundation.compatibility.ObjectHelper;
 
 /**
  *  
- 
+ Represents a link to another property.  References can be used to specify that
+ two properties on different objects are in fact, the same property.  This also
+ has the added benefit of cutting down on CZML file size.
  
 
  */
@@ -19,13 +22,13 @@ public class Reference implements IEquatable<Reference>, ImmutableValueType {
 
 	/**
 	 *  
-	
+	Creates a new instance.
 	
 	
 	
 
-	 * @param id 
-	 * @param path 
+	 * @param id The id of the object which contains the referenced property.
+	 * @param path The path to the property in the referenced object.
 	 */
 	public Reference(String id, String path) {
 		m_identifier = id;
@@ -33,8 +36,7 @@ public class Reference implements IEquatable<Reference>, ImmutableValueType {
 	}
 
 	/**
-	 *  
-	
+	 *  Gets the id of the object which contains the referenced property.
 	
 
 	 */
@@ -43,8 +45,7 @@ public class Reference implements IEquatable<Reference>, ImmutableValueType {
 	}
 
 	/**
-	 *  
-	
+	 *  Gets the path to the property in the referenced object.
 	
 
 	 */
@@ -54,25 +55,50 @@ public class Reference implements IEquatable<Reference>, ImmutableValueType {
 
 	/**
 	 *  
-	
+	Indicates whether another object is exactly equal to this instance.
 	
 	
 	
 
-	 * @param other 
+	 * @param obj The object to compare to this instance.
+	 * @return <see langword="true" /> if <code>obj</code> is an instance of this type and represents the same value as this instance; otherwise, <see langword="false" />.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Reference) {
+			return equalsType((Reference) obj);
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 *  
+	Indicates whether another object is exactly equal to this instance.
+	
+	
+	
+
+	 * @param other The object to compare to this instance.
+	 * @return <see langword="true" /> if <code>other</code> is an instance of this type and represents the same value as this instance; otherwise, <see langword="false" />.
 	 */
 	public final boolean equalsType(Reference other) {
-		throw new UnsupportedOperationException();
+		return ObjectHelper.equals(this.m_identifier, other.m_identifier) && ObjectHelper.equals(this.m_path, other.m_path);
+	}
+
+	/**
+	 *  
+	Returns a hash code for this instance, which is suitable for use in hashing algorithms and data structures like a hash table.
+	
+	
+
+	 * @return A hash code for the current object.
+	 */
+	@Override
+	public int hashCode() {
+		return m_identifier.hashCode() ^ m_path.hashCode();
 	}
 
 	private String m_identifier;
 	private String m_path;
-
-	@Override
-	public int hashCode() {
-		int result = 17;
-		result = 31 * result + (m_identifier == null ? 0 : m_identifier.hashCode());
-		result = 31 * result + (m_path == null ? 0 : m_path.hashCode());
-		return result;
-	}
 }
