0d972f8e36e88d059ca8255e6e38c4a12b92dada
Ed Mackey <elm19087@gmail.com>
2014-09-22 14:11:40 -0400

    Changes after review.

--- DotNet/ExampleCesiumLanguageServer/Properties/AssemblyInfo.cs
+++ DotNet/ExampleCesiumLanguageServer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExampleCesiumLanguageServer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014 Cesium Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
