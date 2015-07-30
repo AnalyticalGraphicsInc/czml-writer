0d083ecc09fbd434b98dfe4a8115717d8d4f9bdf
Ed Mackey <elm19087@gmail.com>
2015-07-30 11:13:46 -0400

    Add three more materials to polylines.

--- Java/CesiumLanguageWriter/translatedSrc/cesiumlanguagewriter/PolylineMaterialCesiumWriter.java
+++ Java/CesiumLanguageWriter/translatedSrc/cesiumlanguagewriter/PolylineMaterialCesiumWriter.java
@@ -5,9 +5,12 @@ import agi.foundation.compatibility.*;
 import agi.foundation.compatibility.Func1;
 import agi.foundation.compatibility.Lazy;
 import cesiumlanguagewriter.advanced.*;
+import cesiumlanguagewriter.GridMaterialCesiumWriter;
+import cesiumlanguagewriter.ImageMaterialCesiumWriter;
 import cesiumlanguagewriter.PolylineGlowMaterialCesiumWriter;
 import cesiumlanguagewriter.PolylineOutlineMaterialCesiumWriter;
 import cesiumlanguagewriter.SolidColorMaterialCesiumWriter;
+import cesiumlanguagewriter.StripeMaterialCesiumWriter;
 
 /**
  *  
@@ -37,6 +40,27 @@ public class PolylineMaterialCesiumWriter extends CesiumPropertyWriter<PolylineM
 
 	 */
 	public static final String PolylineGlowPropertyName = "polylineGlow";
+	/**
+	 *  
+	The name of the <code>image</code> property.
+	
+
+	 */
+	public static final String ImagePropertyName = "image";
+	/**
+	 *  
+	The name of the <code>grid</code> property.
+	
+
+	 */
+	public static final String GridPropertyName = "grid";
+	/**
+	 *  
+	The name of the <code>stripe</code> property.
+	
+
+	 */
+	public static final String StripePropertyName = "stripe";
 	private Lazy<SolidColorMaterialCesiumWriter> m_solidColor = new Lazy<cesiumlanguagewriter.SolidColorMaterialCesiumWriter>(new Func1<cesiumlanguagewriter.SolidColorMaterialCesiumWriter>() {
 		public cesiumlanguagewriter.SolidColorMaterialCesiumWriter invoke() {
 			return new SolidColorMaterialCesiumWriter(SolidColorPropertyName);
@@ -51,6 +75,21 @@ public class PolylineMaterialCesiumWriter extends CesiumPropertyWriter<PolylineM
 	private Lazy<PolylineGlowMaterialCesiumWriter> m_polylineGlow = new Lazy<cesiumlanguagewriter.PolylineGlowMaterialCesiumWriter>(new Func1<cesiumlanguagewriter.PolylineGlowMaterialCesiumWriter>() {
 		public cesiumlanguagewriter.PolylineGlowMaterialCesiumWriter invoke() {
 			return new PolylineGlowMaterialCesiumWriter(PolylineGlowPropertyName);
+		}
+	}, false);
+	private Lazy<ImageMaterialCesiumWriter> m_image = new Lazy<cesiumlanguagewriter.ImageMaterialCesiumWriter>(new Func1<cesiumlanguagewriter.ImageMaterialCesiumWriter>() {
+		public cesiumlanguagewriter.ImageMaterialCesiumWriter invoke() {
+			return new ImageMaterialCesiumWriter(ImagePropertyName);
+		}
+	}, false);
+	private Lazy<GridMaterialCesiumWriter> m_grid = new Lazy<cesiumlanguagewriter.GridMaterialCesiumWriter>(new Func1<cesiumlanguagewriter.GridMaterialCesiumWriter>() {
+		public cesiumlanguagewriter.GridMaterialCesiumWriter invoke() {
+			return new GridMaterialCesiumWriter(GridPropertyName);
+		}
+	}, false);
+	private Lazy<StripeMaterialCesiumWriter> m_stripe = new Lazy<cesiumlanguagewriter.StripeMaterialCesiumWriter>(new Func1<cesiumlanguagewriter.StripeMaterialCesiumWriter>() {
+		public cesiumlanguagewriter.StripeMaterialCesiumWriter invoke() {
+			return new StripeMaterialCesiumWriter(StripePropertyName);
 		}
 	}, false);
 
@@ -139,5 +178,65 @@ public class PolylineMaterialCesiumWriter extends CesiumPropertyWriter<PolylineM
 	public final PolylineGlowMaterialCesiumWriter openPolylineGlowProperty() {
 		openIntervalIfNecessary();
 		return this.<PolylineGlowMaterialCesiumWriter> openAndReturn(getPolylineGlowWriter());
+	}
+
+	/**
+	 *  Gets the writer for the <code>image</code> property.  The returned instance must be opened by calling the  {@link CesiumElementWriter#open} method before it can be used for writing.  The <code>image</code> property defines fills the line with an image.
+	
+
+	 */
+	public final ImageMaterialCesiumWriter getImageWriter() {
+		return m_image.getValue();
+	}
+
+	/**
+	 *  
+	Opens and returns the writer for the <code>image</code> property.  The <code>image</code> property defines fills the line with an image.
+	
+
+	 */
+	public final ImageMaterialCesiumWriter openImageProperty() {
+		openIntervalIfNecessary();
+		return this.<ImageMaterialCesiumWriter> openAndReturn(getImageWriter());
+	}
+
+	/**
+	 *  Gets the writer for the <code>grid</code> property.  The returned instance must be opened by calling the  {@link CesiumElementWriter#open} method before it can be used for writing.  The <code>grid</code> property defines fills the line with a grid.
+	
+
+	 */
+	public final GridMaterialCesiumWriter getGridWriter() {
+		return m_grid.getValue();
+	}
+
+	/**
+	 *  
+	Opens and returns the writer for the <code>grid</code> property.  The <code>grid</code> property defines fills the line with a grid.
+	
+
+	 */
+	public final GridMaterialCesiumWriter openGridProperty() {
+		openIntervalIfNecessary();
+		return this.<GridMaterialCesiumWriter> openAndReturn(getGridWriter());
+	}
+
+	/**
+	 *  Gets the writer for the <code>stripe</code> property.  The returned instance must be opened by calling the  {@link CesiumElementWriter#open} method before it can be used for writing.  The <code>stripe</code> property defines fills the line with alternating colors.
+	
+
+	 */
+	public final StripeMaterialCesiumWriter getStripeWriter() {
+		return m_stripe.getValue();
+	}
+
+	/**
+	 *  
+	Opens and returns the writer for the <code>stripe</code> property.  The <code>stripe</code> property defines fills the line with alternating colors.
+	
+
+	 */
+	public final StripeMaterialCesiumWriter openStripeProperty() {
+		openIntervalIfNecessary();
+		return this.<StripeMaterialCesiumWriter> openAndReturn(getStripeWriter());
 	}
 }
