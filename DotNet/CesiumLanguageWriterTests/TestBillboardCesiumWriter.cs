7574c115654f63315bb20a2dc53be39c5994c11d
Scott Hunter <shunter@agi.com>
2016-06-08 19:39:24 -0400

    Add some additional tests.

---
+++ DotNet/CesiumLanguageWriterTests/TestBillboardCesiumWriter.cs
@@ -0,0 +1,15 @@
+﻿using CesiumLanguageWriter;
+using CesiumLanguageWriter.Advanced;
+using NUnit.Framework;
+
+namespace CesiumLanguageWriterTests
+{
+    [TestFixture]
+    public class TestBillboardCesiumWriter : TestCesiumPropertyWriter<BillboardCesiumWriter>
+    {
+        protected override CesiumPropertyWriter<BillboardCesiumWriter> CreatePropertyWriter(string propertyName)
+        {
+            return new BillboardCesiumWriter(propertyName);
+        }
+    }
+}
---
+++ DotNet/CesiumLanguageWriterTests/TestHorizontalOriginCesiumWriter.cs
@@ -0,0 +1,42 @@
+using CesiumLanguageWriter;
+using CesiumLanguageWriter.Advanced;
+using NUnit.Framework;
+
+namespace CesiumLanguageWriterTests
+{
+    [TestFixture]
+    public class TestHorizontalOriginCesiumWriter : TestCesiumPropertyWriter<HorizontalOriginCesiumWriter>
+    {
+        [Test]
+        public void HorizontalOriginCanBeWrittenAsSimpleString()
+        {
+            using (Packet)
+            using (HorizontalOriginCesiumWriter writer = new HorizontalOriginCesiumWriter("foo"))
+            {
+                writer.Open(OutputStream);
+                writer.WriteHorizontalOrigin(CesiumHorizontalOrigin.Center);
+            }
+            Assert.AreEqual("{\"foo\":\"CENTER\"}", StringWriter.ToString());
+        }
+
+        [Test]
+        public void HorizontalOriginCanBeWrittenInsideInterval()
+        {
+            JulianDate startDate = new GregorianDate(2012, 6, 7, 12, 0, 0).ToJulianDate();
+
+            using (Packet)
+            using (HorizontalOriginCesiumWriter writer = new HorizontalOriginCesiumWriter("foo"))
+            {
+                writer.Open(OutputStream);
+                writer.WriteInterval(startDate, startDate.AddSeconds(100.0));
+                writer.WriteHorizontalOrigin(CesiumHorizontalOrigin.Center);
+            }
+            Assert.AreEqual("{\"foo\":{\"interval\":\"20120607T12Z/20120607T120140Z\",\"horizontalOrigin\":\"CENTER\"}}", StringWriter.ToString());
+        }
+
+        protected override CesiumPropertyWriter<HorizontalOriginCesiumWriter> CreatePropertyWriter(string propertyName)
+        {
+            return new HorizontalOriginCesiumWriter(propertyName);
+        }
+    }
+}
