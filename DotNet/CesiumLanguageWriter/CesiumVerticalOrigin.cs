0a3ceaf7bdc59c79eacd738a14d3beb4e5f3846b
Scott Hunter <shunter@agi.com>
2016-07-14 16:56:13 -0400

    update doc

--- DotNet/CesiumLanguageWriter/CesiumVerticalOrigin.cs
+++ DotNet/CesiumLanguageWriter/CesiumVerticalOrigin.cs
@@ -1,22 +1,22 @@
 ﻿namespace CesiumLanguageWriter
 {
     /// <summary>
-    /// The vertical origin of a billboard or label relative to its position.
+    /// The vertical origin of an item relative to its position.
     /// </summary>
     public enum CesiumVerticalOrigin
     {
         /// <summary>
-        /// The bottom of the billboard or label is located at the position.
+        /// The bottom of the item is located at the position.
         /// </summary>
         Bottom,
 
         /// <summary>
-        /// The billboard or label is vertically centered at the position.
+        /// The item is vertically centered at the position.
         /// </summary>
         Center,
 
         /// <summary>
-        /// The top of the billboard or label is located at the position.
+        /// The top of the item is located at the position.
         /// </summary>
         Top,
     }
