21f949ab35ab8f18a2e9da2e204f8d21a377c55f
Scott Hunter <shunter@agi.com>
2017-03-14 11:49:28 -0400

    Date formatting changed in Windows 10.  tweak tests to allow old or new format.

--- DotNet/CesiumLanguageWriterTests/TestGregorianDateFormatting.cs
+++ DotNet/CesiumLanguageWriterTests/TestGregorianDateFormatting.cs
@@ -35,8 +35,8 @@ namespace CesiumLanguageWriterTests
             Assert.AreEqual("5:25 AM", date.ToString("t", m_cultureInfo));
             Assert.AreEqual("5:25:13 AM", date.ToString("T", m_cultureInfo));
             Assert.AreEqual("2002-02-25 05:25:13Z", date.ToString("u", m_cultureInfo));
-            Assert.AreEqual("February, 2002", date.ToString("y", m_cultureInfo));
-            Assert.AreEqual("February, 2002", date.ToString("Y", m_cultureInfo));
+            CollectionAssert.Contains(new[] { "February, 2002", "February 2002" }, date.ToString("y", m_cultureInfo));
+            CollectionAssert.Contains(new[] { "February, 2002", "February 2002" }, date.ToString("Y", m_cultureInfo));
             Assert.AreEqual("2/25/2002 5:25:13 AM", date.ToString("", m_cultureInfo));
             Assert.AreEqual("25", date.ToString("%d", m_cultureInfo));
             Assert.AreEqual("25", date.ToString("dd", m_cultureInfo));
