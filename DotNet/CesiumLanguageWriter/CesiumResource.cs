8cfb0e786550ac47baa273c699f9a62054ea822f
Scott Hunter <shunter@agi.com>
2017-05-17 13:54:21 -0400

    Add methods to create a CesiumResource from an in-memory image, or a stream.

--- DotNet/CesiumLanguageWriter/CesiumResource.cs
+++ DotNet/CesiumLanguageWriter/CesiumResource.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
+using CesiumLanguageWriter.Advanced;
 
 namespace CesiumLanguageWriter
 {
@@ -7,7 +10,7 @@ namespace CesiumLanguageWriter
     /// </summary>
     public class CesiumResource
     {
-        private readonly string m_url;
+        private readonly string m_uri;
         private readonly CesiumResourceBehavior m_behavior;
 
         /// <summary>
@@ -27,7 +30,7 @@ namespace CesiumLanguageWriter
         /// <param name="behavior">The enumeration describing how the resource is to be included in the document.</param>
         public CesiumResource(string uri, CesiumResourceBehavior behavior)
         {
-            m_url = uri;
+            m_uri = uri;
             m_behavior = behavior;
         }
 
@@ -36,7 +39,7 @@ namespace CesiumLanguageWriter
         /// </summary>
         public string Uri
         {
-            get { return m_url; }
+            get { return m_uri; }
         }
 
         /// <summary>
@@ -45,6 +48,45 @@ namespace CesiumLanguageWriter
         public CesiumResourceBehavior Behavior
         {
             get { return m_behavior; }
+        }
+
+        /// <summary>
+        /// Create a CZML resource from an image loaded into memory.  The image data will 
+        /// be embedded in the CZML document using a data URI.
+        /// </summary>
+        /// <param name="image">The image to write.</param>
+        /// <param name="imageFormat">The format of the image.</param>
+        /// <returns>A new <see cref="CesiumResource"/> containing the image data as a data URI.</returns>
+        public static CesiumResource FromImage(Image image, CesiumImageFormat imageFormat)
+        {
+            string dataUri = CesiumFormattingHelper.ImageToDataUri(image, imageFormat);
+            return new CesiumResource(dataUri, CesiumResourceBehavior.Embed);
+        }
+
+        /// <summary>
+        /// Create a CZML resource from an stream containing image data.  The image data will 
+        /// be embedded in the CZML document using a data URI.
+        /// </summary>
+        /// <param name="stream">The stream containing image data to write.</param>
+        /// <param name="imageFormat">The format of the image.</param>
+        /// <returns>A new <see cref="CesiumResource"/> containing the image data as a data URI.</returns>
+        public static CesiumResource FromStream(Stream stream, CesiumImageFormat imageFormat)
+        {
+            string dataUri = CesiumFormattingHelper.ImageToDataUri(stream, imageFormat);
+            return new CesiumResource(dataUri, CesiumResourceBehavior.Embed);
+        }
+
+        /// <summary>
+        /// Create a CZML resource from an stream.  The data will 
+        /// be embedded in the CZML document using a data URI.
+        /// </summary>
+        /// <param name="stream">The stream containing data to write.</param>
+        /// <param name="mimeType">The mime type of the data.</param>
+        /// <returns>A new <see cref="CesiumResource"/> containing the data as a data URI.</returns>
+        public static CesiumResource FromStream(Stream stream, string mimeType)
+        {
+            string dataUri = CesiumFormattingHelper.StreamToDataUri(stream, mimeType);
+            return new CesiumResource(dataUri, CesiumResourceBehavior.Embed);
         }
     }
 }
---
+++ DotNet/CesiumLanguageWriterTests/TestCesiumResource.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using CesiumLanguageWriter;
+using NUnit.Framework;
+
+namespace CesiumLanguageWriterTests
+{
+    [TestFixture]
+    public class TestCesiumResource
+    {
+        [Test]
+        public void TestFromImage()
+        {
+            var image = new Bitmap(5, 5);
+            var resource = CesiumResource.FromImage(image, CesiumImageFormat.Png);
+
+            Assert.IsNotNull(resource);
+
+            StringAssert.StartsWith("data:image/png;base64,", resource.Uri);
+        }
+
+        [Test]
+        [TestCaseSource("ImageFormatValues")]
+        public void TestFromImageFormats(CesiumImageFormat format)
+        {
+            var image = new Bitmap(5, 5);
+            var resource = CesiumResource.FromImage(image, format);
+
+            Assert.IsNotNull(resource);
+
+            StringAssert.StartsWith("data:", resource.Uri);
+            StringAssert.Contains(";base64,", resource.Uri);
+        }
+
+        public IEnumerable<CesiumImageFormat> ImageFormatValues
+        {
+            get { return (CesiumImageFormat[])Enum.GetValues(typeof(CesiumImageFormat)); }
+        }
+
+        [Test]
+        public void TestFromStream()
+        {
+            // arbitrary bytes
+            byte[] contents = { 0, 1, 2, 3, 4 };
+            var stream = new MemoryStream(contents);
+
+            var resource = CesiumResource.FromStream(stream, CesiumImageFormat.Png);
+
+            Assert.IsNotNull(resource);
+
+            const string expected = "data:image/png;base64,AAECAwQ=";
+            Assert.AreEqual(expected, resource.Uri);
+        }
+
+        [Test]
+        public void TestFromStreamMimeType()
+        {
+            // arbitrary bytes
+            byte[] contents = { 0, 1, 2, 3, 4 };
+            var stream = new MemoryStream(contents);
+
+            var resource = CesiumResource.FromStream(stream, "application/octet-stream");
+
+            Assert.IsNotNull(resource);
+
+            const string expected = "data:application/octet-stream;base64,AAECAwQ=";
+            Assert.AreEqual(expected, resource.Uri);
+        }
+    }
+}
