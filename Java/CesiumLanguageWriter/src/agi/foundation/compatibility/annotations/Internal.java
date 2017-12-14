ada1885b39c916715ce8a555745276a561829f73
Scott Hunter <shunter@agi.com>
2017-12-14 10:18:36 -0500

    include @Internal annotation in javadoc.

--- Java/CesiumLanguageWriter/src/agi/foundation/compatibility/annotations/Internal.java
+++ Java/CesiumLanguageWriter/src/agi/foundation/compatibility/annotations/Internal.java
@@ -1,6 +1,9 @@
 package agi.foundation.compatibility.annotations;
 
+import java.lang.annotation.Documented;
+
 /**
  * Marker interface for declarations that were internal in C#.
  */
+@Documented
 public @interface Internal {}
