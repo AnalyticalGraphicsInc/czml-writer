1b0b789606f3fdb80cb133eddbd9af08abb799ad
Scott Hunter <shunter@agi.com>
2017-12-14 10:48:58 -0500

    use try-with-resources in sandbox example code.

--- Java/CesiumLanguageWriterTests/src/cesiumlanguagewritertests/Sandbox.java
+++ Java/CesiumLanguageWriterTests/src/cesiumlanguagewritertests/Sandbox.java
@@ -1,11 +1,5 @@
 package cesiumlanguagewritertests;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.net.URI;
-
-import org.junit.Test;
-
 import cesiumlanguagewriter.BillboardCesiumWriter;
 import cesiumlanguagewriter.Cartographic;
 import cesiumlanguagewriter.CesiumOutputStream;
@@ -13,32 +7,34 @@ import cesiumlanguagewriter.CesiumResourceBehavior;
 import cesiumlanguagewriter.CesiumStreamWriter;
 import cesiumlanguagewriter.PacketCesiumWriter;
 import cesiumlanguagewriter.PositionCesiumWriter;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URI;
 
 public class Sandbox {
     @Test
-    public void go() throws IOException {
-        StringWriter sw = new StringWriter();
-
-        CesiumOutputStream output = new CesiumOutputStream(sw);
+    public void runExample() throws IOException {
+        StringWriter stringWriter = new StringWriter();
+        CesiumOutputStream output = new CesiumOutputStream(stringWriter);
         output.setPrettyFormatting(true);
 
         CesiumStreamWriter stream = new CesiumStreamWriter();
-        PacketCesiumWriter packet = stream.openPacket(output);
+        try (PacketCesiumWriter packet = stream.openPacket(output)) {
+            packet.writeId("Test");
 
-        packet.writeId("Test");
+            try (BillboardCesiumWriter billboard = packet.openBillboardProperty()) {
+                billboard.writeColorProperty(123, 67, 0, 255);
+                billboard.writeImageProperty(URI.create("http://cesiumjs.org/images/CesiumHeaderLogo.png"), CesiumResourceBehavior.EMBED);
+            }
 
-        BillboardCesiumWriter billboard = packet.openBillboardProperty();
-        billboard.writeColorProperty(123, 67, 0, 255);
-        billboard.writeImageProperty(URI.create("http://cesiumjs.org/images/CesiumHeaderLogo.png"), CesiumResourceBehavior.EMBED);
-        billboard.close();
+            try (PositionCesiumWriter position = packet.openPositionProperty()) {
+                position.writeCartographicDegrees(new Cartographic(-75.0, 45.0, 100.0));
+            }
+        }
+        stringWriter.close();
 
-        PositionCesiumWriter position = packet.openPositionProperty();
-        position.writeCartographicDegrees(new Cartographic(-75.0, 45.0, 100.0));
-        position.close();
-
-        packet.close();
-        sw.close();
-
-        System.out.println(sw.toString());
+        System.out.println(stringWriter.toString());
     }
 }
