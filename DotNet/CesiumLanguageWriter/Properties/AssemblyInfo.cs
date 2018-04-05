ea9efbb4a78396dba49936fc5e3c79fbc308f4d9
Scott Hunter <shunter@agi.com>
2018-04-05 14:16:48 -0400

    update date in assembly copyright

--- DotNet/CesiumLanguageWriter/Properties/AssemblyInfo.cs
+++ DotNet/CesiumLanguageWriter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Analytical Graphics, Inc.")]
 [assembly: AssemblyProduct("CesiumLanguageWriter")]
-[assembly: AssemblyCopyright("Copyright © 2011-2017 Cesium Contributors")]
+[assembly: AssemblyCopyright("Copyright © 2011-2018 Cesium Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
