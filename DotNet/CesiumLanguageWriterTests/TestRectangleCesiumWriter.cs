82c46ccc9c7b57ecd42a101fe652a9717507bf97
Scott Hunter <shunter@agi.com>
2019-04-30 16:19:40 -0400

    Add heightReference/extrudedHeightReference for types that were missing.

---
+++ DotNet/CesiumLanguageWriterTests/TestRectangleCesiumWriter.cs
@@ -0,0 +1,29 @@
+﻿using CesiumLanguageWriter;
+using CesiumLanguageWriter.Advanced;
+using NUnit.Framework;
+
+namespace CesiumLanguageWriterTests
+{
+    [TestFixture]
+    public class TestRectangleCesiumWriter : TestCesiumPropertyWriter<RectangleCesiumWriter>
+    {
+        [Test]
+        public void TestHeightReferenceProperties()
+        {
+            using (Packet)
+            using (var rectangle = Packet.OpenRectangleProperty())
+            using (var interval = rectangle.OpenInterval())
+            {
+                interval.WriteExtrudedHeightProperty(10.0);
+                interval.WriteExtrudedHeightReferenceProperty(CesiumHeightReference.RelativeToGround);
+            }
+
+            Assert.AreEqual("{\"rectangle\":{\"extrudedHeight\":10,\"extrudedHeightReference\":\"RELATIVE_TO_GROUND\"}}", StringWriter.ToString());
+        }
+
+        protected override CesiumPropertyWriter<RectangleCesiumWriter> CreatePropertyWriter(string propertyName)
+        {
+            return new RectangleCesiumWriter(propertyName);
+        }
+    }
+}
