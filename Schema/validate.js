b853f67659d974ea55933528d5b438ba55e1f9af
Scott Hunter <shunter@agi.com>
2019-05-15 16:07:49 -0400

    Modernize validate script.

--- Schema/validate.js
+++ Schema/validate.js
@@ -1,34 +1,26 @@
-var fs = require('fs');
-var glob = require('glob');
-var Ajv = require('ajv');
+const fs = require('fs');
+const util = require('util');
+const Ajv = require('ajv');
+const readFileAsync = util.promisify(fs.readFile);
 
 function requireJSON(path) {
     return JSON.parse(fs.readFileSync(path, 'utf8'));
 }
 
-var schemas = glob.sync('*.json', { ignore: 'package.json' }).concat(glob.sync('Extensions/**/*.json')).map(function (filename) {
-    return requireJSON(filename);
-});
+function loadSchema(uri) {
+    uri = uri.replace('https://analyticalgraphicsinc.github.io/czml-writer/Schema/', '');
+    return readFileAsync(uri, 'utf8').then(JSON.parse);
+}
 
 var ajv = new Ajv({
     allErrors: true,
     verbose: true,
-    schemas: schemas
+    loadSchema: loadSchema
 });
 
 var schema = requireJSON('./Packet.json');
 
-var validate = ajv.compile(schema);
-
-function test(path) {
-    var packet = requireJSON(path);
-    var valid = validate(packet);
-    if (!valid) {
-        console.log('File ' + path + ' Invalid: ' + ajv.errorsText(validate.errors));
-    }
-}
-
-[
+var filesToValidate = [
     'Examples/Packet.json',
     'Examples/DeletePacket.json',
     'Examples/DocumentPacket.json',
@@ -41,7 +33,20 @@ function test(path) {
     'Examples/SimplePosition.json',
     'Examples/TimeVaryingPosition.json',
     'Examples/ViewFrom.json'
-].forEach(function (f) {
-    console.log('Validating ' + f);
-    test(f);
+];
+
+ajv.compileAsync(schema).then(validate => {
+
+    filesToValidate.forEach(path => {
+        console.log(`Validating ${path}...`);
+
+        const packet = requireJSON(path);
+        const valid = validate(packet);
+        if (valid) {
+            console.log(`...Valid!`);
+        } else {
+            console.log(`...Invalid: ${ajv.errorsText(validate.errors)}`);
+        }
+    });
+
 });
