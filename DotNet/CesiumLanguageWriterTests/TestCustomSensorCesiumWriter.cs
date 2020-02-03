ca4004fd7c1ceec44ab0a8737031ec94614cae15
Scott Hunter <shunter@agi.com>
2020-02-03 09:20:31 -0500

    Add tests for sensors.

---
+++ DotNet/CesiumLanguageWriterTests/TestCustomSensorCesiumWriter.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using CesiumLanguageWriter;
+using CesiumLanguageWriter.Advanced;
+using NUnit.Framework;
+
+namespace CesiumLanguageWriterTests
+{
+    [TestFixture]
+    public class TestCustomPatternSensorCesiumWriter : TestCesiumPropertyWriter<CustomPatternSensorCesiumWriter>
+    {
+        [Test]
+        public void TestBasicProperties()
+        {
+            const bool expectedShow = true;
+            const double expectedRadius = 1234.5;
+
+            using (Packet)
+            using (var rectangle = Packet.OpenCustomPatternSensorProperty())
+            using (var interval = rectangle.OpenInterval())
+            {
+                interval.WriteShowProperty(expectedShow);
+                interval.WriteRadiusProperty(expectedRadius);
+            }
+
+            AssertExpectedJson(PacketCesiumWriter.CustomPatternSensorPropertyName, new Dictionary<string, object>
+            {
+                { CustomPatternSensorCesiumWriter.ShowPropertyName, expectedShow },
+                { CustomPatternSensorCesiumWriter.RadiusPropertyName, expectedRadius },
+            });
+        }
+
+        [Test]
+        public void TestIntersectionProperties()
+        {
+            const bool expectedShowIntersection = true;
+            Color expectedIntersectionColor = Color.Blue;
+            const double expectedIntersectionWidth = 2.1;
+            const bool expectedShowEnvironmentIntersection = true;
+            Color expectedEnvironmentIntersectionColor = Color.Red;
+
+            using (Packet)
+            using (var rectangle = Packet.OpenCustomPatternSensorProperty())
+            using (var interval = rectangle.OpenInterval())
+            {
+                interval.WriteShowIntersectionProperty(expectedShowIntersection);
+                interval.WriteIntersectionColorProperty(expectedIntersectionColor);
+                interval.WriteIntersectionWidthProperty(expectedIntersectionWidth);
+
+                interval.WriteShowEnvironmentIntersectionProperty(expectedShowEnvironmentIntersection);
+                interval.WriteEnvironmentIntersectionColorProperty(expectedEnvironmentIntersectionColor);
+            }
+
+            AssertExpectedJson(PacketCesiumWriter.CustomPatternSensorPropertyName, new Dictionary<string, object>
+            {
+                { CustomPatternSensorCesiumWriter.ShowIntersectionPropertyName, expectedShowIntersection },
+                { CustomPatternSensorCesiumWriter.IntersectionColorPropertyName, expectedIntersectionColor },
+                { CustomPatternSensorCesiumWriter.IntersectionWidthPropertyName, expectedIntersectionWidth },
+                { CustomPatternSensorCesiumWriter.ShowEnvironmentIntersectionPropertyName, expectedShowEnvironmentIntersection },
+                { CustomPatternSensorCesiumWriter.EnvironmentIntersectionColorPropertyName, expectedEnvironmentIntersectionColor },
+            });
+        }
+
+        [Test]
+        public void TestShowThroughEllipsoid()
+        {
+            const bool expectedShowThroughEllipsoid = true;
+
+            using (Packet)
+            using (var rectangle = Packet.OpenCustomPatternSensorProperty())
+            using (var interval = rectangle.OpenInterval())
+            {
+                interval.WriteShowThroughEllipsoidProperty(expectedShowThroughEllipsoid);
+            }
+
+            AssertExpectedJson(PacketCesiumWriter.CustomPatternSensorPropertyName, new Dictionary<string, object>
+            {
+                { CustomPatternSensorCesiumWriter.ShowThroughEllipsoidPropertyName, expectedShowThroughEllipsoid },
+            });
+        }
+
+        protected override CesiumPropertyWriter<CustomPatternSensorCesiumWriter> CreatePropertyWriter(string propertyName)
+        {
+            return new CustomPatternSensorCesiumWriter(propertyName);
+        }
+    }
+}
---
+++ DotNet/CesiumLanguageWriterTests/TestRectangularSensorCesiumWriter.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using CesiumLanguageWriter;
+using CesiumLanguageWriter.Advanced;
+using NUnit.Framework;
+
+namespace CesiumLanguageWriterTests
+{
+    [TestFixture]
+    public class TestRectangularSensorCesiumWriter : TestCesiumPropertyWriter<RectangularSensorCesiumWriter>
+    {
+        [Test]
+        public void TestBasicProperties()
+        {
+            const bool expectedShow = true;
+            const double expectedXHalfAngle = 1.1;
+            const double expectedYHalfAngle = 1.2;
+            const double expectedRadius = 1234.5;
+
+            using (Packet)
+            using (var rectangle = Packet.OpenRectangularSensorProperty())
+            using (var interval = rectangle.OpenInterval())
+            {
+                interval.WriteShowProperty(expectedShow);
+                interval.WriteXHalfAngleProperty(expectedXHalfAngle);
+                interval.WriteYHalfAngleProperty(expectedYHalfAngle);
+                interval.WriteRadiusProperty(expectedRadius);
+            }
+
+            AssertExpectedJson(PacketCesiumWriter.RectangularSensorPropertyName, new Dictionary<string, object>
+            {
+                { RectangularSensorCesiumWriter.ShowPropertyName, expectedShow },
+                { RectangularSensorCesiumWriter.XHalfAnglePropertyName, expectedXHalfAngle },
+                { RectangularSensorCesiumWriter.YHalfAnglePropertyName, expectedYHalfAngle },
+                { RectangularSensorCesiumWriter.RadiusPropertyName, expectedRadius },
+            });
+        }
+
+        [Test]
+        public void TestIntersectionProperties()
+        {
+            const bool expectedShowIntersection = true;
+            Color expectedIntersectionColor = Color.Blue;
+            const double expectedIntersectionWidth = 2.1;
+            const bool expectedShowEnvironmentIntersection = true;
+            Color expectedEnvironmentIntersectionColor = Color.Red;
+
+            using (Packet)
+            using (var rectangle = Packet.OpenRectangularSensorProperty())
+            using (var interval = rectangle.OpenInterval())
+            {
+                interval.WriteShowIntersectionProperty(expectedShowIntersection);
+                interval.WriteIntersectionColorProperty(expectedIntersectionColor);
+                interval.WriteIntersectionWidthProperty(expectedIntersectionWidth);
+
+                interval.WriteShowEnvironmentIntersectionProperty(expectedShowEnvironmentIntersection);
+                interval.WriteEnvironmentIntersectionColorProperty(expectedEnvironmentIntersectionColor);
+            }
+
+            AssertExpectedJson(PacketCesiumWriter.RectangularSensorPropertyName, new Dictionary<string, object>
+            {
+                { RectangularSensorCesiumWriter.ShowIntersectionPropertyName, expectedShowIntersection },
+                { RectangularSensorCesiumWriter.IntersectionColorPropertyName, expectedIntersectionColor },
+                { RectangularSensorCesiumWriter.IntersectionWidthPropertyName, expectedIntersectionWidth },
+                { RectangularSensorCesiumWriter.ShowEnvironmentIntersectionPropertyName, expectedShowEnvironmentIntersection },
+                { RectangularSensorCesiumWriter.EnvironmentIntersectionColorPropertyName, expectedEnvironmentIntersectionColor },
+            });
+        }
+
+        [Test]
+        public void TestShowThroughEllipsoid()
+        {
+            const bool expectedShowThroughEllipsoid = true;
+
+            using (Packet)
+            using (var rectangle = Packet.OpenRectangularSensorProperty())
+            using (var interval = rectangle.OpenInterval())
+            {
+                interval.WriteShowThroughEllipsoidProperty(expectedShowThroughEllipsoid);
+            }
+
+            AssertExpectedJson(PacketCesiumWriter.RectangularSensorPropertyName, new Dictionary<string, object>
+            {
+                { RectangularSensorCesiumWriter.ShowThroughEllipsoidPropertyName, expectedShowThroughEllipsoid },
+            });
+        }
+
+        protected override CesiumPropertyWriter<RectangularSensorCesiumWriter> CreatePropertyWriter(string propertyName)
+        {
+            return new RectangularSensorCesiumWriter(propertyName);
+        }
+    }
+}
